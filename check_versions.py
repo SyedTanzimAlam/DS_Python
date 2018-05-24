ef085e3208a2e2e217b83b6ab64faba8e8719080
SyedTanzimAlam <syedtanzimalam88@yahoo.com>
2018-05-24 06:21:21 +0530

    Basics of Python

---
+++ check_versions.py
@@ -0,0 +1,22 @@
+"""@author: Tanzim"""
+#TO CHECK VERSION OF THE LIBRARIES WE WANT TO IMPORT
+
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
