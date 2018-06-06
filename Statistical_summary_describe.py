fd991eca29c5feecffd272ba2547b6b7b0f22504
SyedTanzimAlam <syedtanzimalam88@yahoo.com>
2018-06-06 12:05:45 +0530

    Statistical summary

--- Statistical_summary_describe.py
+++ Statistical_summary_describe.py
@@ -2,7 +2,7 @@
 # Statistical Summary
 import pandas as pd
 filename = "PUT THE .csv file"
-colnames = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+colnames = ['Column names in quotes seperated by comma']
 dataset = pd.read_csv(filename, names=colnames)
 pd.set_option('display.width', 100)
 pd.set_option('precision', 3)
